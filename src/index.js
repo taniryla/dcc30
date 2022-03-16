ea192fd9746ad21229cdc1522ac6f5451c414c27
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 10:54:14 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 30-totalTaskTime
+Difficulty:  Difficult
+Prompt:
+- Write a function called totalTaskTime that accepts two arguments.
+- The first argument is an array of integers referred to as a "queue".  Each integer in the queue represents a "task", more specifically, the amount of time to complete that task.
+- The second argument is an integer representing the number of CPU "threads" available to process all of the tasks in the queue.
+- The totalTaskTime function should return an integer representing the total time it is going to take to complete all of the tasks in the queue.
+- You may mutate the "queue" array (first argument) if you wish.
+Hint:
+- Solve it with paper and pencil first.  Look for patterns and generalize.  Pseudocode!
+Examples:
+totalTaskTime( [], 1 ) // => 0
+totalTaskTime( [4, 2, 5], 1 ) // => 11
+totalTaskTime( [5, 8], 2 ) // => 8
+totalTaskTime( [4, 2, 10], 2 ) // => 12
+totalTaskTime( [2, 2, 3, 3, 4, 4], 2 ) //=> 9
+totalTaskTime( [5, 2, 6, 8, 7, 2], 3 ) // => 12
+-----------------------------------------------------------------*/
+// Your solution for 30- here:
+
+</div>
+`;
